806d12698134a7a14ca5f62fa45e8d5acbce3b25
frederic.juppont <frederic.juppont@gmail.com>
2019-08-20 18:32:33 +0200

    unicodecode

--- robot/utils.py
+++ robot/utils.py
@@ -1,18 +1,11 @@
 from .words import WORDSLIST
-from textwrap import wrap
-# import unidecode
+import unidecode
 
 
 def parse(question):
     words = [word for word in question.split() if word not in WORDSLIST]
-    # for ind, word in enumerate(words):
-    #     word = unidecode.unidecode(word)
-    #     words[ind] = word
+    for ind, word in enumerate(words):
+        word = unidecode.unidecode(word)
+        words[ind] = word
     return words
 
-def parse_text_wiki(d):
-    res = ''
-    for k in d:
-        if 'extract' in d[k]:
-            res += d[k]['extract']
-    return wrap(res, width=70)
